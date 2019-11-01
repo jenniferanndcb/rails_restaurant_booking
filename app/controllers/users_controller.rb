cb9d2f90e8e74cac0f9d4567260c604673be15bd
Jen <jenniferanndcb@gmail.com>
2019-11-01 10:09:57 +0000

    sign up and user show page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController 
+
+  def new 
+    @user = User.new 
+  end 
+
+  def create 
+    user = User.create(user_params)
+
+    if user.valid? 
+      session[:user_id] = user.id 
+      redirect_to user_path(user)
+    else 
+      redirect_to new_user_path 
+    end   
+  end
+
+  def show 
+    @user = User.find_by(:id => params[:id])
+  end 
+  
+  
+  private 
+
+  def user_params 
+    params.require(:user).permit(:first_name,:email,:password,:admin)
+  end 
+end 
