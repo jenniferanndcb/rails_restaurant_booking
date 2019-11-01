80fdbd0352e99d08008ffeb81f3a5dc06bd7968b
Jen <jenniferanndcb@gmail.com>
2019-11-01 10:15:59 +0000

    bookings and restaurant ctlr

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,10 @@
+class BookingsController < ApplicationController 
+
+  def new 
+    @booking = Booking.new 
+  end 
+
+  def create 
+
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do 
+    resources :bookings, only: [:new, :create] 
+  end 
+    
 
   root "welcome#home"
 
