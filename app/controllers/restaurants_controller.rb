2b2c2ba0c32ce64ca82a26de28b9e9566062eb7b
Jen <jenniferanndcb@gmail.com>
2019-11-04 06:20:30 +0000

    restaurant create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,43 @@
 class RestaurantsController < ApplicationController 
+  before_action :set_restaurant, only: [:show, :edit, :update]
 
   def new
     @restaurant = Restaurant.new  
   end 
   
   def create 
-
+    restaurant = Restaurant.create(restaurant_params)
+    if restaurant.valid? 
+      redirect_to restaurant_path(restaurant)
+    else 
+      redirect_to new_restaurant_path 
+    end 
   end 
 
   def show 
+    # @restaurant = Restaurant.find_by(id: params[:id])
+  end 
+
+  def edit 
+    # @restaurant = Restaurant.find_by(id: params[:id])
+  end 
+
+  def update 
+    # @restaurant = Restaurant.find_by(id: params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else 
+      redirect_to edit_restaurant_path(@restaurant)
+    end 
+  end 
+
+  private 
+  
+  def set_restaurant 
     @restaurant = Restaurant.find_by(id: params[:id])
+  end 
+
+  def restaurant_params 
+    params.require(:restaurant).permit(:name,:location,:website,:phone_number,:opening_hrs)
   end 
 end 
